c6de0575bc6cbfc30287b7d1e384e47ed41c3240
Роман Нарожнов <rommka_83@mail.ru>
2023-04-13 20:10:27 +0300

    установил storybook

--- src/shared/ui/RightArrow/RightArrow.tsx
+++ src/shared/ui/RightArrow/RightArrow.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import styles from './rightarrow.module.css';
 
 export function RightArrow() {
-  return <button className={styles.RightArrow}></button>;
+  return (
+    <button className={styles.RightArrow}>
+      <svg
+        viewBox='0 0 512 512'
+        xmlns='http://www.w3.org/2000/svg'
+        xmlnsXlink='http://www.w3.org/1999/xlink'
+        fill='var(--white)'
+      >
+        <polygon points='160,128.4 192.3,96 352,256 352,256 352,256 192.3,416 160,383.6 287.3,256 ' />
+      </svg>
+    </button>
+  );
 }
