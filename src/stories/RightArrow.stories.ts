f3b48b95a8d4cf507ed0f06b28991434f3f2c334
Роман Нарожнов <rommka_83@mail.ru>
2023-04-13 21:01:57 +0300

    добавил глобальные стили к storybook

---
+++ src/stories/RightArrow.stories.ts
@@ -0,0 +1,44 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { RightArrow } from '../shared/ui/RightArrow';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Example/RightArrow',
+  component: RightArrow,
+  tags: ['autodocs'],
+  //   argTypes: {
+  //     backgroundColor: { control: 'color' },
+  //   },
+} satisfies Meta<typeof RightArrow>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Primary: Story = {
+  //   args: {
+  //     primary: true,
+  //     label: 'Button',
+  //   },
+};
+
+// export const Secondary: Story = {
+//   args: {
+//     label: 'Button',
+//   },
+// };
+
+// export const Large: Story = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+
+// export const Small: Story = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
