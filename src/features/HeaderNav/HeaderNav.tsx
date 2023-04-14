1ecdf1c6e1ed039b5a97588741b2dc16d1b02d6c
@KsuSmolyar <ibragimova-ksenija@lenta.ru>
2023-04-14 15:56:16 +0400

    Добавлена фича HeaderNav

---
+++ src/features/HeaderNav/HeaderNav.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./headernav.module.css";
+import { Link } from "shared/ui/Link/Link";
+import { useTranslation } from "react-i18next";
+
+export function HeaderNav() {
+	const { t } = useTranslation();
+	return (
+		<nav className={styles.nav}>
+			<ul className={styles.list}>
+				<li className={styles.item}>
+					<Link to='/'>{t("navList.Home")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/MovieList'>{t("navList.MovieList")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/MoviePage'>{t("navList.MoviePage")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/ActorPage'>{t("navList.ActorPage")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/Authorization'>{t("navList.Authorization")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/MovieReviews'>{t("navList.MovieReviews")}</Link>
+				</li>
+				<li className={styles.item}>
+					<Link to='/AdminPanel'>{t("navList.AdminPanel")}</Link>
+				</li>
+			</ul>
+		</nav>
+	);
+}
--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,40 +1,13 @@
-import React from 'react';
-import styles from './header.module.css';
-import { Link } from 'react-router-dom';
-import { useTranslation } from 'react-i18next';
-import { ChangeTheLanguage } from 'features/ChangeTheLanguage';
+import React from "react";
+import styles from "./header.module.css";
+import { ChangeTheLanguage } from "features/ChangeTheLanguage";
+import { HeaderNav } from "features/HeaderNav";
 
 export function Header() {
-  const { t } = useTranslation();
-
-  return (
-    <header className={styles.header}>
-      <nav className={styles.nav}>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <Link to='/'>{t('navList.Home')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/MovieList'>{t('navList.MovieList')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/MoviePage'>{t('navList.MoviePage')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/ActorPage'>{t('navList.ActorPage')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/Authorization'>{t('navList.Authorization')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/MovieReviews'>{t('navList.MovieReviews')}</Link>
-          </li>
-          <li className={styles.item}>
-            <Link to='/AdminPanel'>{t('navList.AdminPanel')}</Link>
-          </li>
-        </ul>
-      </nav>
-      <ChangeTheLanguage />
-    </header>
-  );
+	return (
+		<header className={styles.header}>
+			<HeaderNav />
+			<ChangeTheLanguage />
+		</header>
+	);
 }
