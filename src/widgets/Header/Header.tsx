d93764c280b07f7e9f3f865f2769fafcece734a9
@KsuSmolyar <ibragimova-ksenija@lenta.ru>
2023-04-18 13:58:23 +0400

    удален импорт реакта

--- src/widgets/Header/Header.tsx
+++ src/widgets/Header/Header.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import styles from "./header.module.css";
 import { ChangeTheLanguage } from "features/ChangeTheLanguage";
 import { HeaderNav } from "features/HeaderNav";
