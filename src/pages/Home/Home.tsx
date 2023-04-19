e9c5d665a07ce5ef357d163a58c04a2e86c35481
Your Name <you@example.com>
2023-04-19 11:44:25 +0300

    commit

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,6 +3,7 @@ import styles from './home.module.css';
 import test from '../../temp/DB/test.json';
 import { IFilm } from 'entities/MovieBadge';
 import { CategoryFilms } from 'widgets/CategoryFilms';
+import { PromoSlider } from 'widgets/PromoSlider';
 
 let [obj, obj2] = test.movies;
 let arr: IFilm[] = Array(20).fill(obj);
@@ -10,9 +11,13 @@ let arr2: IFilm[] = Array(20).fill(obj2);
 
 export function Home() {
   return (
-    <div className='container'>
-      <CategoryFilms title={obj.category} movies={arr} />
-      <CategoryFilms title={obj2.category} movies={arr2} />
-    </div>
+    <>
+      <PromoSlider movies={test.movies} />
+      <div className='container'>
+        <CategoryFilms title={obj.category} movies={arr} />
+        <CategoryFilms title={obj2.category} movies={arr2} />
+      </div>
+    </>
+
   );
 }
