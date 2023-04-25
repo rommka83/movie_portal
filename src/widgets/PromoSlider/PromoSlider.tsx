1397151962e87823a179440e3c041f3523056a59
Your Name <you@example.com>
2023-04-25 11:55:45 +0300

    fix slider

--- src/widgets/PromoSlider/PromoSlider.tsx
+++ src/widgets/PromoSlider/PromoSlider.tsx
@@ -5,32 +5,34 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation } from 'swiper';
+import { Autoplay, Navigation } from 'swiper';
 
 import { PromoSlide } from 'entities/PromoSlide';
 import { IFilm } from 'shared/types/IFilm';
+import { UseMedia } from 'shared/hooks/useMedia';
 
 type PropsType = {
   movies: IFilm[];
 };
 
 export const PromoSlider: React.FC<PropsType> = ({ movies }) => {
+
+  const brakePoint = UseMedia('(max-width:768px)')
+
+
   return (
     <Swiper
-      // Autoplay
-      modules={[Navigation]}
+      modules={[Navigation, Autoplay]}
       spaceBetween={20}
-      slidesPerView={1.23}
-      navigation
+      slidesPerView={1.25}
+      navigation={!brakePoint}
       loop
       centeredSlides
-      // centeredSlides={false}
-      // centerInsufficientSlides={true}
-      /* autoplay={{
+      autoplay={{
         delay: 8000,
         disableOnInteraction: false,
         pauseOnMouseEnter: true,
-      }} */
+      }}
       className={styles.slider}
     >
       {movies.map((movie) => (
